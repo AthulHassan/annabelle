00359c424e5dc650cc6263e6b33111b3da30ba5b
AthulHassan <97149526+AthulHassan@users.noreply.github.com>
2022-01-05 12:41:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AthulHassan/annabelle /EvaMaria
-else
+  git clone https://github.com/AthulHassan/annabelle /NETFLIXINDIA
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AthulHassan
  
